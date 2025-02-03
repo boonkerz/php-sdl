e53ba363e38f9e0c8cff598c2ae4670e5b6949d6
X3N0N <fusionphenom@gmail.com>
2025-02-03 10:27:26 +0200

    Update php_sdl.c to retrieve SDL3/SDL3_img/SDL3_ttf version of the linked library.

--- php_sdl.c
+++ php_sdl.c
@@ -69,23 +69,28 @@ PHP_RINIT_FUNCTION(sdl)
 
 PHP_MINFO_FUNCTION(sdl)
 {
-    int compiled = SDL_VERSION;
-    int linked = SDL_GetVersion();
-    char compiled_version_str[32];
-    char linked_version_str[32];
+    int sdl_linked = SDL_GetVersion();
+	int img_linked = IMG_Version();
+	int ttf_linked = TTF_Version();
+    char buff[32];
 
     php_info_print_table_start();
+	
     php_info_print_table_header(2, "SDL support", "enabled");
 
     php_info_print_table_row(2, "SDL module version", PHP_SDL_VERSION);
 
-    snprintf(compiled_version_str, sizeof(compiled_version_str), "%d.%d.%d",
-             SDL_VERSIONNUM_MAJOR(compiled), SDL_VERSIONNUM_MINOR(compiled), SDL_VERSIONNUM_MICRO(compiled));
-    php_info_print_table_row(2, "SDL headers version", compiled_version_str);
-
-    snprintf(linked_version_str, sizeof(linked_version_str), "%d.%d.%d",
-             SDL_VERSIONNUM_MAJOR(linked), SDL_VERSIONNUM_MINOR(linked), SDL_VERSIONNUM_MICRO(linked));
-    php_info_print_table_row(2, "SDL library version", linked_version_str);
+    snprintf(buff, sizeof(buff), "%d.%d.%d",
+             SDL_VERSIONNUM_MAJOR(sdl_linked), SDL_VERSIONNUM_MINOR(sdl_linked), SDL_VERSIONNUM_MICRO(sdl_linked));
+    php_info_print_table_row(2, "SDL library version", buff);
+	
+    snprintf(buff, sizeof(buff), "%d.%d.%d",
+             SDL_VERSIONNUM_MAJOR(img_linked), SDL_VERSIONNUM_MINOR(img_linked), SDL_VERSIONNUM_MICRO(img_linked));
+    php_info_print_table_row(2, "SDL_image library version", buff);
+	
+    snprintf(buff, sizeof(buff), "%d.%d.%d",
+             SDL_VERSIONNUM_MAJOR(ttf_linked), SDL_VERSIONNUM_MINOR(ttf_linked), SDL_VERSIONNUM_MICRO(ttf_linked));
+    php_info_print_table_row(2, "SDL_ttf library version", buff);
 
     php_info_print_table_end();
 }
